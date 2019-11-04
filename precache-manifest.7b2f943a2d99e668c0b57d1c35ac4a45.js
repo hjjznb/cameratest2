44805cc9d558b00c71ffd4ef4419795aaf0d37f8
hujiajun <huajiajun@sensetime.com>
2019-11-04 12:40:22 +0800

    first commit

---
+++ precache-manifest.7b2f943a2d99e668c0b57d1c35ac4a45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49657dc7be7c7d2f9480",
+    "url": "/css/app.81157f11.css"
+  },
+  {
+    "revision": "49657dc7be7c7d2f9480",
+    "url": "/js/app.42d8331c.js"
+  },
+  {
+    "revision": "7a85659624eed566a6cd",
+    "url": "/js/chunk-vendors.a48b8ef9.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ac97183d95dc0d617d6929b390e4d219",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
